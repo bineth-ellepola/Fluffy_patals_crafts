f8133434b8a9d037064487555a1c75e94a1db681
Bineth <binethellepola@gmail.com>
2026-01-09 23:52:55 +0530

    Install tailnwind package and implement the Home,jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import ProductGallery from "../components/ProductGallery";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <ProductGallery />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E9689A",   // pink
+        secondary: "#A958C8", // purple
+        accent: "#F8D57E",    // yellow
+        leaf: "#61C487"       // green
+      }
+    },
+  },
+  plugins: [],
+}
